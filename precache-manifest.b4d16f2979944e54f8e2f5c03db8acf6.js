6d02a42b6927b5525e854288e749192281e3da7e
Vipin Kumar <vipikuma@redhat.com>
2020-03-08 11:28:01 +0530

    Updates

---
+++ precache-manifest.b4d16f2979944e54f8e2f5c03db8acf6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41e9686eea157377c273d2d3d4f93a2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d22bacceca9bf9d9015d",
+    "url": "/static/css/main.415253a9.chunk.css"
+  },
+  {
+    "revision": "2371a437ee26137b6601",
+    "url": "/static/js/2.ef30b1ce.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.ef30b1ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d22bacceca9bf9d9015d",
+    "url": "/static/js/main.e44d12dd.chunk.js"
+  },
+  {
+    "revision": "2ecda4bc27d965187554",
+    "url": "/static/js/runtime-main.cc8d3cab.js"
+  },
+  {
+    "revision": "d48527036615b77671bd513127e36276",
+    "url": "/static/media/back_ground.d4852703.jpg"
+  }
+]);
