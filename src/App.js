31c5a662843c603c5d38d228f2394433fdca1a9c
Vipin Kumar <vipikuma@redhat.com>
2020-03-08 14:38:26 +0530

    update and all URLS working perfect now

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Nav from './navigation';
+import Admission from './admission';
+import Class from './class';
+import Competitive from './competitive';
+import Home from './home'
+import Contect from './contect';
+import Download from './downloads';
+import Program from './programs';
+import Why from './why_us';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>THE PATH WAY ACADEMY</h1>
-      <div>
-      <table className="menu_list">
-        <tr>
-          <td>Programs</td>
-          <td>Competitive Exam</td>
-          <td>Class Schedule</td>
-          <td>Downloads</td>
-          <td>Admission</td>
-          <td>Contect US</td>
-          <td>Why US?</td>
-        </tr>
-      </table>
-      </div>
-    </div>
+    <Router>
+      <Nav />
+      <Switch>
+        <div className="App">
+          <Route path="/" exext component={Home} />
+          <Route path="/admission" component={Admission} />
+          <Route path="/class" component={Class} />
+          <Route path="/competitive" component={Competitive} />
+          <Route path="/contect" component={Contect} />
+          <Route path="/downloads" component={Download} />
+          <Route path="/programs" component={Program} />
+          <Route path="/why_us" component={Why} />
+        </div>
+      </Switch>
+    </Router>
   );
 }
 
