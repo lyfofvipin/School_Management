0db3d63f82866d3dd3658ba382870ba61f83cd9b
Vipin Kumar <vipikuma@redhat.com>
2020-03-10 17:44:09 +0530

    Done with Why Us page

--- src/why_us.js
+++ src/why_us.js
@@ -3,9 +3,24 @@ import './App.css';
 
 function WhyUs() {
   return (
-    <div className="App">
-      <h1>Why Us?</h1>
-    </div>
+    <p className="why_us">
+      <h2>Best teachers :-</h2>
+      From IITS four other top colleges with 5 + year experience. 
+
+      <h2>Adaptive teaching  :-</h2>
+      Delivering customized learning based on Students pace. 
+
+      <h2>Leave for interactive :-</h2>
+      Teaching with the concept of visualisation through smart classrooms. <br />
+      Shared live learning environment between student and teacher. <br />
+      Learning management tools that essentially includes teaching learning context creation sharing and evaluation.<br />
+      Live session on mobile devices.<br />
+      Performance reporting for student and parents. <br />
+      <br />
+      <br />
+      <br />
+      We are constantly working on more efficient solution that will drive the evolution fundamentally changing the pattern of teaching and learning today.
+    </p>
   );
 }
 
