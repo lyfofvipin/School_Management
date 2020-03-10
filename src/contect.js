1ac5702b970045dccd8eebb8e7baee0185aac9a7
Vipin Kumar <vipikuma@redhat.com>
2020-03-10 16:01:06 +0530

    Done with contect Us page

--- src/contect.js
+++ src/contect.js
@@ -3,8 +3,14 @@ import './App.css';
 
 function Contect() {
   return (
-    <div className="App">
-      <h1>Contect Us.</h1>
+    <div className="App" className="contect">
+      <p className="batch">
+          #B.J-20 <br />
+          Beside Canra Bank <br />
+          Shalimar Bagh, New Delhi <br />
+          Ph:- +91 9958557743 <br />
+          E-Mail:- thepathwayacademy11@gmail.com <br />
+        </p>
     </div>
   );
 }
